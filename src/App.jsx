52205deffda46820edda1ffa28daef1177156aa3
Supriya Kavathekar <supriyakavathekar20@gmail.com>
2023-08-14 11:46:50 +0200

    added router and created home components and also home.css Co-authored-by: MaagMichael <MaagMichael@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+// npm i react-router-dom
+
+import React from "react";
+// import react router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./components/Home";
+
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-   
+ 
     </div>
   );
 }
 
 export default App;
+
