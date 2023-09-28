13aa949bf27bc591462234354d3b22c675bae0f9
Supriya Kavathekar <supriyakavathekar20@gmail.com>
2023-09-28 16:09:53 +0200

    added section and heading for about hero section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,20 @@ import './About.css';
 function About() {
   return (
     
-<h1>test</h1>
+<div>
 
+<section id='about-hero' className='about-hero' >
+
+{/* about background image */}
+<div className='about-hero-image'></div>
+<div className='home-navbar'></div>
+<div> <h1>ABOUT US</h1> </div>
+
+</section>
+
+
+
+</div>
 
 
 
